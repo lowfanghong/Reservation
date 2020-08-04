e2a096d83a95625afa1676b6ae9d1ab86bf0d8db
lowfanghong <18021531@myrp.edu.sg>
2020-08-04 22:27:06 +0800

    final commit

--- app/src/main/java/sg/edu/rp/c346_18021531/reservation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346_18021531/reservation/MainActivity.java
@@ -1,8 +1,13 @@
 package sg.edu.rp.c346_18021531.reservation;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -12,12 +17,13 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
-  EditText etName,etMobile,etGroupSize;
+  EditText etName,etMobile,etGroupSize,etDay,etTime;
   CheckBox cbSmoking;
-  Button btnSubmit, btnDefault,btnReset;
-    DatePicker dp;
-    TimePicker tp;
+  Button btnSubmit, btnReset;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +31,47 @@ public class MainActivity extends AppCompatActivity {
         etName = findViewById(R.id.editText10);
         etMobile = findViewById(R.id.editText2);
         etGroupSize = findViewById(R.id.editText3);
+        etDay = findViewById(R.id.editTextday);
+        etTime = findViewById(R.id.editTextTime);
         cbSmoking = findViewById(R.id.checkBox2);
         btnSubmit = findViewById(R.id.button);
-        btnDefault = findViewById(R.id.button3);
         btnReset = findViewById(R.id.button2);
-        tp = findViewById(R.id.timePicker);
-        tp.setIs24HourView(true);
-        dp = findViewById(R.id.datePicker);
+
+     etDay.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+                 @Override
+                 public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                     etDay.setText("Date:" + dayOfMonth + "/" + (month+1) + "/" + year);
+                 }
+             };
+             Calendar CA = Calendar.getInstance();
+             DatePickerDialog myDateDialog = new DatePickerDialog(MainActivity.this,
+                     myDateListener,CA.get(Calendar.YEAR),CA.get(Calendar.MONTH),
+                     CA.get(Calendar.DAY_OF_MONTH));
+             myDateDialog.show();
+         }
+     });
+     etTime.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             TimePickerDialog.OnTimeSetListener myTimeListener = new
+                     TimePickerDialog.OnTimeSetListener() {
+                         @Override
+                         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                             etTime.setText("Time: " + hourOfDay +":" + minute);
+                         }
+                     };
+
+             Calendar CA = Calendar.getInstance();
+             TimePickerDialog myTimePickerDialog =
+                     new TimePickerDialog(MainActivity.this,
+                             myTimeListener,CA.get(Calendar.HOUR),CA.get(Calendar.MINUTE),
+                             false);
+             myTimePickerDialog.show();
+         }
+     });
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,42 +79,63 @@ public class MainActivity extends AppCompatActivity {
         String name = etName.getText().toString();
         String mb = etMobile.getText().toString();
         String GS = etGroupSize.getText().toString();
-                tp.setIs24HourView(true);
-                Integer currenthour = tp.getCurrentHour();
-                Integer currentminute  = tp.getCurrentMinute();
-                Integer day = dp.getDayOfMonth();
-                Integer month = dp.getMonth() + 1;
-                Integer year = dp.getYear();
+        String day = etDay.getText().toString();
+        String time = etTime.getText().toString();
+
 
                 if (cbSmoking.isChecked()){
-                    Toast.makeText(MainActivity.this,
-                            name + " has booked a smoking area with "
-                                    + GS +" people on "+
-                                    day + "/"+ month + "/" +
-                                    year +" with timing of "+ currenthour + ":"
-                                    + currentminute + "." + name
-                                    +"'s mobile phone number is " + mb,
-                            Toast.LENGTH_LONG).show();
+                    LayoutInflater inflater = (LayoutInflater) getBaseContext().getSystemService
+                            (Context.LAYOUT_INFLATER_SERVICE);
+                    View viewDialog3 = inflater.inflate(R.layout.row,null);
+                    final TextView tvName = viewDialog3.findViewById(R.id.textViewName);
+                    final TextView tvSmoking = viewDialog3.findViewById(R.id.textViewSmoking);
+                    final TextView tvSize = viewDialog3.findViewById(R.id.textViewSize);
+                    final TextView tvDate = viewDialog3.findViewById(R.id.textViewDate);
+                    final TextView tvTime = viewDialog3.findViewById(R.id.textViewTime);
+                    tvName.setText("Name: " + name);
+                    tvSmoking.setText("Smoking: Smoking ");
+                    tvSize.setText("Size: " + GS);
+                    tvDate.setText("Date: " + day);
+                    tvTime.setText("Time: " + time);
+                    AlertDialog.Builder myBuilder = new AlertDialog.Builder
+                            (MainActivity.this);
+                    myBuilder.setView(viewDialog3);
+                    myBuilder.setTitle("Confirm Your Order");
+                    myBuilder.setCancelable(false);
+                    myBuilder.setPositiveButton("confirm",null);
+                    myBuilder.setNeutralButton("Cancel",null);
+                    AlertDialog myDialog = myBuilder.create();
+                    myDialog.show();
+
+
                 }else{
-                    Toast.makeText(MainActivity.this,
-                            name + " has booked a non-smoking area with " + GS
-                                    +" people on "+
-                                    day + "/"+ month + "/" +
-                                    year +" with timing of "+ currenthour + ":"
-                                    + currentminute + "." + name
-                                    +"'s mobile phone number is " + mb,
-                            Toast.LENGTH_LONG).show();
+                    LayoutInflater inflater = (LayoutInflater) getBaseContext().getSystemService
+                            (Context.LAYOUT_INFLATER_SERVICE);
+                    View viewDialog3 = inflater.inflate(R.layout.row,null);
+                    final TextView tvName = viewDialog3.findViewById(R.id.textViewName);
+                    final TextView tvSmoking = viewDialog3.findViewById(R.id.textViewSmoking);
+                    final TextView tvSize = viewDialog3.findViewById(R.id.textViewSize);
+                    final TextView tvDate = viewDialog3.findViewById(R.id.textViewDate);
+                    final TextView tvTime = viewDialog3.findViewById(R.id.textViewTime);
+                    tvName.setText("Name: " + name);
+                    tvSmoking.setText("Smoking: Non-Smoking ");
+                    tvSize.setText("Size: " + GS);
+                    tvDate.setText("Date: " + day);
+                    tvTime.setText("Time: " + time);
+                    AlertDialog.Builder myBuilder = new AlertDialog.Builder
+                            (MainActivity.this);
+                    myBuilder.setView(viewDialog3);
+                    myBuilder.setTitle("Confirm Your Order");
+                    myBuilder.setCancelable(false);
+                    myBuilder.setPositiveButton("confirm",null);
+                    myBuilder.setNeutralButton("Cancel",null);
+                    AlertDialog myDialog = myBuilder.create();
+                    myDialog.show();
+
                 }
             }
         });
-        btnDefault.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tp.setCurrentHour(19);
-                tp.setCurrentMinute(30);
-                dp.updateDate(2020,5,1);
-            }
-        });
+
         btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -82,9 +143,9 @@ public class MainActivity extends AppCompatActivity {
             etName.setText("");
                 etMobile.setText("");
                 etGroupSize.setText("");
-                tp.setCurrentHour(19);
-                tp.setCurrentMinute(30);
-                dp.updateDate(2020,5,1);
+                etDay.setText("");
+                etTime.setText("");
+
             }
         });
     }
